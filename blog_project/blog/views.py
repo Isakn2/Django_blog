69b09995f19429979480f4ad81e5c98b8b1c28cf
Isaac Silva <isak.silva81@gmail.com>
2025-02-18 00:06:37 -0600

    1st commit

---
+++ blog_project/blog/views.py
@@ -0,0 +1,52 @@
+# blog/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from .forms import PostForm
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.author != request.user:
+        return HttpResponseForbidden()
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form})
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.author != request.user:
+        return HttpResponseForbidden()
+    post.delete()
+    return redirect('post_list')
+
+
+# Create your views here.
